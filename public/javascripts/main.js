85dd7c4412bc3ab98df1a844789d57e725eaed8c
Chang Liu <shampliu@gmail.com>
2016-01-16 17:11:05 -0800

    ui changes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,23 +1,42 @@
 $(document).ready(function() {
+
 	Myo.connect('com.myojs.main');
 
-	var audio = new Audio('javascripts/arctic.mp3');
+	var audio1 = new Audio('javascripts/clap.wav');
 	var audio2 = new Audio('javascripts/hihat.wav');
 	var audio3 = new Audio('javascripts/snare.wav')
 
 	Myo.on('fist', function(){
-	    audio3.play();
-	    this.vibrate();
-	    $('#bass').css("display", "block");
-	});
+		audio3.play();
+		$('#bass').css("display", "block");
+		$('#pad1').css("background-color", "red");
+		setTimeout(function() {
+			$('#pad1').css("background-color", "white")
 
+		}, 600)
+	}, function() {
+
+	});
 	Myo.on('wave_in', function() {
-		audio1.play();
+		
 	})
 
 	Myo.on('wave_out', function() {
 		audio2.play();
+		$('#pad2').css("background-color", "blue");
+		setTimeout(function() {
+			$('#pad2').css("background-color", "white")
+
+		}, 600)
 	})
+	Myo.on('fingers_spread', function(){
+		audio1.play()
+		$('#pad3').css("background-color", "green");
+		setTimeout(function() {
+			$('#pad3').css("background-color", "white")
+
+		}, 600)
+	});
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -10,4 +10,16 @@ router.get('/main', function(req, res, next) {
   res.render('main');
 });
 
+router.get('/main2', function(req, res, next) {
+  res.render('main2');
+});
+
+router.get('/main3', function(req, res, next) {
+  res.render('main3');
+});
+
+router.get('/main4', function(req, res, next) {
+  res.render('main4');
+});
+
 module.exports = router;
